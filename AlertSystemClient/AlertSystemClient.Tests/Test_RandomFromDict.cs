f7537c2fe0dd0fddda12c16692880a22fe9058c2
Priyanka-Priyadarshini <53027439+Priyanka-Priyadarshini@users.noreply.github.com>
2019-08-02 10:48:14 +0530

    Add files via upload

---
+++ AlertSystemClient/AlertSystemClient.Tests/Test_RandomFromDict.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AlertSystemClient.Tests
+{
+    [TestFixture]
+   
+    public class Test_RandomFromDict
+    {
+        RandomFromDict r1;
+        [SetUp]
+        public void Init()
+        {
+            r1 = new RandomFromDict();
+        }
+        
+        // test for creating dictionary form xml...
+        [TestCase(@"C:\Users\320066905\source\repos\AlertSystemClient\AlertSystemClient\bin\Debug\PatientData.xml", "SPO2", ExpectedResult = 8)]
+        [TestCase(@"C:\Users\320066905\source\repos\AlertSystemClient\AlertSystemClient\bin\Debug\PatientData.xml", "Pulse_rate_value", ExpectedResult = 8)]
+        [TestCase(@"C:\Users\320066905\source\repos\AlertSystemClient\AlertSystemClient\bin\Debug\PatientData.xml", "Temperature_Value", ExpectedResult = 8)]
+        public int Test_ParseXmlData(string pathXml,string tag)
+        {
+            Dictionary<string, string> dict_SPO2 = r1.ParseXmlData(pathXml, tag);
+            return (dict_SPO2.Count());
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            r1 = null;
+        }
+    }
+}
